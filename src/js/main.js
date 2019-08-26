4f16924c955a1d63e8cbe30d9cebbaa4313bbd78
Pawel Ruszkiewicz <protech@pt.home.pl>
2019-08-26 15:06:45 +0200

    feat: adding submenu

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,11 @@ const switchMenu = document.querySelector('.mainSwither__js');
 switchMenu.addEventListener('click', (e) => {
     const navigationList = document.querySelector('.navigation__cont--contUl-js');
 
-    if(navigationList.classList.toggle('navigation__cont--contUl-visible')){
-        switchMenu.innerHTML='<span style="color: red;">x</span>';
+    if (navigationList.classList.toggle('navigation__cont--contUl-visible')) {
+        switchMenu.innerHTML = '<span style="color: red;">x</span>';
     }
-    else{
-        switchMenu.innerHTML="&#9776";
+    else {
+        switchMenu.innerHTML = "&#9776";
     };
-
-})
-// obsluga submenu po klik, rozwijane menu
-const menuMain = document.querySelectorAll('.navigation__cont--item--a');
-
-menuMain.addEventListener('click', (e) =>{
-    const menuSub = document.querySelectorAll('.submenu');
-     menuSub.classList.toggle('submenu-js');
 
 })
